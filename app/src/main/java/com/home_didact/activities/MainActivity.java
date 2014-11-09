9f42cfebbd046256ad81fa24f9cf62b007e4456c
aidanb <aidanb@gmail.com>
2014-11-10 00:04:10 +1100

    Added new learner activity.

---
+++ app/src/main/java/com/home_didact/activities/MainActivity.java
@@ -0,0 +1,51 @@
+package com.home_didact.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.home_didact.R;
+
+
+public class MainActivity extends Activity {
+
+
+    /*
+    Load the new learner activity
+     */
+    public void loadNewLearnerActivity(View view) {
+        Intent intent = new Intent(this, NewLearnerActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/home_didact/activities/NewLearnerActivity.java
+++ app/src/main/java/com/home_didact/activities/NewLearnerActivity.java
@@ -1,24 +1,25 @@
-package com.home_didact;
+package com.home_didact.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.home_didact.R;
 
-public class MainActivity extends Activity {
+public class NewLearnerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_learner);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_new_learner, menu);
         return true;
     }
 
