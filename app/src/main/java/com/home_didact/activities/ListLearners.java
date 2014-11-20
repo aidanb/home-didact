11f3bfbd6c5c14be7416235bd5b01c1b7f08569c
aidanb <aidanb@gmail.com>
2014-11-20 15:00:43 +1100

    Fixed intent constructor bug.

--- app/src/main/java/com/home_didact/activities/ListLearners.java
+++ app/src/main/java/com/home_didact/activities/ListLearners.java
@@ -73,7 +73,7 @@ public class ListLearners extends Activity {
             tr.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(this, LearnerProfile.class);
+                    Intent intent = new Intent(view.getContext(), LearnerProfile.class);
                     startActivity(intent);
                 }
             });
